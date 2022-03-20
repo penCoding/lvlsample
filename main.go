c62b76d68d0ff435b743b04982088cd11da27a68
penCoding <fmfchris@gmail.com>
2022-03-20 14:08:38 -0300

    database connection and query successful

--- main.go
+++ main.go
@@ -1,10 +1,13 @@
 package main
 
 import (
+	"database/sql"
+	"fmt"
 	"net/http"
 	"os"
 
 	"github.com/labstack/echo"
+	_ "github.com/mattn/go-sqlite3"
 )
 
 func main() {
@@ -16,8 +19,20 @@ func main() {
 
 	e := echo.New()
 
+	database, _ := sql.Open("sqlite3", "./Chinook_Sqlite.db")
+	rows, _ := database.Query("Select name FROM Track")
+	var name string
+	for rows.Next(){
+		rows.Scan(&name)
+		fmt.Println("Name: " + name)
+	}
+
+
 	e.GET("/tracks", func(c echo.Context) error {
 		return c.String(http.StatusOK, "GET Landing")
 	})
+
+	e.Logger.Print("Listening on port 4041")
+	e.Logger.Fatal(e.Start(fmt.Sprintf("localhost:%s", port)))
 
 }
